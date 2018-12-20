1b91ad9abc09e294eb8a405bbcb3f643051ff0e2
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2018-12-20 16:45:34 +0530

    hello LKM for the R Pi: comments and printk changes
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

--- ch2/cross/hello.c
+++ ch2/cross/hello.c
@@ -1,5 +1,5 @@
 /*
- * ch2/lkm1/hello.c
+ * ch2/cross/hello.c
  ***************************************************************
  * This program is part of the source code released for the book
  *  "Linux Kernel Development Cookbook"
@@ -14,6 +14,10 @@
  * Our very first kernel module, the 'Hello, world' of course! The
  * idea being to explain the essentials of the Linux kernel's LKM
  * framework.
+ * This time, we're attempting to cross compile and run it on the
+ * Raspberry Pi SBC! This does not affect the code in any way, of course,
+ * but *does* affect the Makefile. Check out the new and enhanced Makefile
+ * carefully.
  *
  * For details, please refer the book, Ch 2.
  */
@@ -21,19 +25,19 @@
 #include <linux/module.h>
 
 MODULE_AUTHOR("<insert your name here>");
-MODULE_DESCRIPTION("LKDC book:ch2/lkm1: hello, world, our first LKM");
+MODULE_DESCRIPTION("LKDC book:ch2/cross: hello, world, our first R Pi LKM");
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_VERSION("0.1");
 
 static int __init hello_lkdc_init(void)
 {
-	printk(KERN_INFO "Hello, world\n");
+	printk(KERN_INFO "Hello, Raspberry Pi world\n");
 	return 0;		/* success */
 }
 
 static void __exit hello_lkdc_exit(void)
 {
-	printk(KERN_INFO "Goodbye, world\n");
+	printk(KERN_INFO "Goodbye, Raspberry Pi world\n");
 }
 
 module_init(hello_lkdc_init);
