b7d407fb466a1d1ef8508902c680b80841c98354
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2018-12-29 07:53:05 +0530

    ch2/min_sysinfo/min_sysinfo.c: security: add another ver of the same code, this time impoving security (hopefully); eg. replace the strncat w/ strlcat
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

--- ch2/min_sysinfo/min_sysinfo.c
+++ ch2/min_sysinfo/min_sysinfo.c
@@ -27,6 +27,54 @@ MODULE_DESCRIPTION
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_VERSION("0.1");
 
+static void lkdc_sysinfo2(void)
+{
+#define MSGLEN   128
+	char msg[MSGLEN];
+
+	memset(msg, 0, MSGLEN);
+	snprintf(msg, 48, "%s(): minimal Platform Info:\nCPU: ", __func__);
+
+	/* Strictly speaking, all this #if... is considered ugly and should be
+	   isolated as far as is possible */
+#ifdef CONFIG_X86
+#if(BITS_PER_LONG == 32)
+	strlcat(msg, "x86-32, ", 9);
+#else
+	strlcat(msg, "x86_64, ", 9);
+#endif
+#endif
+#ifdef CONFIG_ARM
+	strlcat(msg, "ARM-32, ", 9);
+#endif
+#ifdef CONFIG_ARM64
+	strlcat(msg, "Aarch64, ", 10);
+#endif
+#ifdef CONFIG_MIPS
+	strlcat(msg, "MIPS, ", 7);
+#endif
+#ifdef CONFIG_PPC
+	strlcat(msg, "PowerPC, ", 10);
+#endif
+#ifdef CONFIG_S390
+	strlcat(msg, "IBM S390, ", 11);
+#endif
+
+#ifdef __BIG_ENDIAN
+	strlcat(msg, "big-endian; ", 13);
+#else
+	strlcat(msg, "little-endian; ", 16);
+#endif
+
+#if(BITS_PER_LONG == 32)
+	strlcat(msg, "32-bit OS.\n", 12);
+#elif(BITS_PER_LONG == 64)
+	strlcat(msg, "64-bit OS.\n", 12);
+#endif
+	pr_info("%s", msg);
+}
+EXPORT_SYMBOL(lkdc_sysinfo2);
+
 static void lkdc_sysinfo(void)
 {
 	char msg[128];
