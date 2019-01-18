ca7deacd6964036ecddff1334b627830957dc964
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-01-18 12:04:26 +0530

    current_affairs.c: refactor the inline code into a small func
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

--- ch4/current_affairs/current_affairs.c
+++ ch4/current_affairs/current_affairs.c
@@ -8,7 +8,7 @@
  *  GitHub repository:
  *  https://github.com/PacktPublishing/Linux-Kernel-Development-Cookbook
  *
- * From: Ch 4: 
+ * From: Ch 4: Memory Allocation for Module Authors
  ****************************************************************
  * Brief Description:
  *
@@ -25,10 +25,9 @@ MODULE_DESCRIPTION("LKDC book:ch/: hello, world");
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_VERSION("0.1");
 
-static int __init current_affairs_init(void)
+static inline void show_ctx(char *nm)
 {
-	pr_debug("%s: inserted\n", OURMODNAME);
-
+	pr_info("%s:%s():%d ", nm, __func__, __LINE__);
 	if (likely(!in_interrupt())) {
 		pr_info(
 		"%s: in process context ::\n"
@@ -37,16 +36,23 @@ static int __init current_affairs_init(void)
 		" name        : %15s\n"
 		" current (ptr to our process context's task_struct) : 0x%pK\n"
 		" stack start : 0x%16pK\n",
-		OURMODNAME, current->pid, current->tgid, current->comm,
+		nm, current->pid, current->tgid, current->comm,
 		current, current->stack);
 	} else
 		pr_alert("%s: in interrupt context [Should NOT Happen here!]\n",
-			OURMODNAME);
+			nm);
+}
+
+static int __init current_affairs_init(void)
+{
+	pr_debug("%s: inserted\n", OURMODNAME);
+	show_ctx(OURMODNAME);
 	return 0;		/* success */
 }
 
 static void __exit current_affairs_exit(void)
 {
+	show_ctx(OURMODNAME);
 	pr_debug("%s: removed\n", OURMODNAME);
 }
 
