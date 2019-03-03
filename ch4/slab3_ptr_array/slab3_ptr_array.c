705adab0cfd82c756367dff23dff58ba9c8e5db7
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-03-03 17:45:28 +0530

    ch4/slab3_ptr_array: assignment, initial commit
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

---
+++ ch4/slab3_ptr_array/slab3_ptr_array.c
@@ -0,0 +1,87 @@
+/*
+ * ch4/slab3_ptr_array/slab3_ptr_array.c
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Linux Kernel Development Cookbook"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *  GitHub repository:
+ *  https://github.com/PacktPublishing/Linux-Kernel-Development-Cookbook
+ *
+ * From: Ch 4 : Kernel Memory Allocation for Module Authors
+ ****************************************************************
+ * Brief Description:
+ * Assignment:
+ * Write a kernel module, that, within it's init code, allocates slab memory
+ * with the kmalloc() to an array of five pointers, i.e., each pointer gets a
+ * kilobyte of slab memory,like this:
+ *   +-------+-------+-------+-------+-------+
+ *   |0  .   |1  .   |2  .   |3  .   |4  .   |
+ *   +---|---+---|---+---|---+---|---+---|---+
+ *       v       v       v       v       v
+ *      ---     ---     ---     ---     ---
+ *      mem     mem     mem     mem     mem       <-- 1 KB each
+ *      ---     ---     ---     ---     ---
+ * Then, initialize the buffers to 'a's, 'b's, 'c's, 'd's and 'e's
+ * respectively.In the cleanup code, free up the memory.
+ * Tip: take care to handle the error case where a memory allocation fails;
+ * don't forget to free the allocations already performed!
+ *
+ * For details, please refer the book, Ch 4.
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+
+#define OURMODNAME   "slab2_correct"
+#define SLAB2_MAXLOOP    5
+
+MODULE_AUTHOR("Kaiwan N Billimoria");
+MODULE_DESCRIPTION("LKDC book:ch4/slab2_correct: fix the bugs in ch4/slab2_buggy");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_VERSION("0.1");
+
+static char *gkptr[SLAB2_MAXLOOP];
+
+static int __init slab2_ptr_array_init(void)
+{
+	int i = 0, j;
+
+	pr_debug("%s: inserted\n", OURMODNAME);
+
+	while (i < SLAB2_MAXLOOP) {
+		gkptr[i] = kmalloc(1024, GFP_KERNEL);
+		if (!gkptr[i]) {
+			pr_warn("%s: kmalloc iter %d failed!\n", OURMODNAME, i);
+			/* Careful! take care to free the prev allocated mem, if any,
+			 * thus avoiding possible memory leakage !
+			 */
+			goto cleanup;
+		}
+		memset(gkptr[i], i+0x61, 1024); // 0x61 = 'a'
+		print_hex_dump_bytes("gkptr: ", DUMP_PREFIX_NONE, gkptr[i], 16);
+		i ++;
+	}
+
+	return 0;		/* success */
+cleanup:
+	for (j=i; j>0; j--) {
+		pr_debug(" freeing gkptr[%d]\n", j);
+		kfree(gkptr[j]);
+	}
+	return -ENOMEM;
+}
+
+static void __exit slab2_ptr_array_cleanup(void)
+{
+	int i;
+	for (i=0; i < SLAB2_MAXLOOP; i++) {
+		pr_debug("%s:%s(): freeing gkptr[%d]\n",
+			OURMODNAME, __func__, i);
+		kfree(gkptr[i]);
+	}
+	pr_debug("%s: freed memory, removed\n", OURMODNAME);
+}
+
+module_init(slab2_ptr_array_init);
+module_exit(slab2_ptr_array_cleanup);
