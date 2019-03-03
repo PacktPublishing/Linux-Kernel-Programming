d5ae4f77a95de9fdfe538b7a6f124af3e8e60cca
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-03-03 17:44:58 +0530

    ch4/slab2_buggy: initial commit
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

---
+++ ch4/slab2_buggy/slab2_buggy.c
@@ -0,0 +1,72 @@
+/*
+ * ch4/slab2_buggy/slab2_buggy.c
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Linux Kernel Development Cookbook"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *  GitHub repository:
+ *  https://github.com/PacktPublishing/Linux-Kernel-Development-Cookbook
+ *
+ * From: Ch 4 : Kernel Memory Allocation for Module Authors
+ ****************************************************************
+ * Brief Description:
+ * If you set the macro BUGGY to 1 (it's 0 by default), then a dangerous bug
+ * surfaces! Check it out - in the init code, on the second loop iteration,
+ * the kmalloc() will not be invoked as the if(kptr == NULL) condition will
+ * evaluate to false; but, the kfree() is invoked, now on an invalid parameter
+ * (it actually becomes a double-free bug)! On our test system, the entire
+ * machine just froze. 
+ *
+ * For details, please refer the book, Ch 4.
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+
+#define OURMODNAME   "slab2_buggy"
+
+MODULE_AUTHOR("Kaiwan N Billimoria");
+MODULE_DESCRIPTION("LKDC book:ch4/slab1: k[m|z]alloc, kfree, basic demo");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_VERSION("0.1");
+
+static int __init slab2_buggy_init(void)
+{
+	int i = 0;
+	char *kptr = NULL;
+
+	pr_debug("%s: inserted\n", OURMODNAME);
+
+#define BUGGY    0   // set to 0 to rm the bug(s)   :-)
+
+	while (++i < 4) {
+#if (BUGGY == 1)
+		if (kptr == NULL)
+#endif
+			kptr = kmalloc(1024, GFP_KERNEL);
+		if (!kptr) {
+			pr_warn("%s: kmalloc failed!\n", OURMODNAME);
+			/* Bug: we don't free the prev allocated mem, if any,
+			 * resulting in possible memory leakage !
+			 */
+			return -ENOMEM;
+		}
+		memset(kptr, i-1+0x61, 1024); // 0x61 = 'a'
+		print_hex_dump_bytes("kptr: ", DUMP_PREFIX_OFFSET, kptr, 16);
+		kfree(kptr);
+#if (BUGGY == 1)
+		pr_info(" %d: kfree on %pK\n", i, kptr);
+#endif
+	}
+
+	return 0;		/* success */
+}
+
+static void __exit slab2_buggy_exit(void)
+{
+	pr_debug("%s: removed\n", OURMODNAME);
+}
+
+module_init(slab2_buggy_init);
+module_exit(slab2_buggy_exit);
