3929daa5d93319644d7fb0daed85d14973be3c03
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-03-16 12:23:33 +0530

    klib_lkdc.h: initial commit

---
+++ klib_lkdc.h
@@ -0,0 +1,27 @@
+/* 
+ * klib_lkdc.h
+ * Our klib_lkdc 'library' header.
+ ****************************************************************
+ * This program is part of the source code released for the book
+ *  "Linux Kernel Development Cookbook"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *  GitHub repository:
+ *  https://github.com/PacktPublishing/Linux-Kernel-Development-Cookbook
+ ****************************************************************
+ * This kernel (module) header code is meant to serve as a 'library' of sorts.
+ * Other kernel modules in our codebase might wish to link into it and use
+ * it's code.
+ *
+ * For details, please refer the book.
+ */
+#ifndef __KLIB_LKDC_H__
+#define __KLIB_LKDC_H__
+
+#include <linux/init.h>
+#include <linux/module.h>
+
+u64 powerof(int base, int exponent);
+void show_phy_pages(const void *kaddr, size_t len, bool contiguity_check);
+
+#endif
