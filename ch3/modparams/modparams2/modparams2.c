a7a3a4fe4efa252fc2e4eb440956e68cad74948c
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-03-31 17:31:18 +0530

    modparams2.c: rename var param 'control1' to 'control_freak' :)

--- ch3/modparams/modparams2/modparams2.c
+++ ch3/modparams/modparams2/modparams2.c
@@ -12,7 +12,7 @@
  ****************************************************************
  * Brief Description:
  * Same as modparams1, plus, we do some checks: here, we propose that
- * the newly introduced parameter 'control1' is mandatory to be passed
+ * the newly introduced parameter 'control_freak' is mandatory to be passed
  * by the user; this module aborts if it isn't explicityl passed along.
  *
  * For details, please refer the book, Ch 3.
@@ -36,32 +36,32 @@ static char *mp_strparam = "My string param";
 module_param(mp_strparam, charp, 0660);
 MODULE_PARM_DESC(mp_strparam, "A demo string parameter");
 
-static int control1;
-module_param(control1, int, 0660);
-MODULE_PARM_DESC(control1, "Set to the project's control level [1-5]. MANDATORY");
+static int control_freak;
+module_param(control_freak, int, 0660);
+MODULE_PARM_DESC(control_freak, "Set to the project's control level [1-5]. MANDATORY");
 
-static int __init modparams1_init(void)
+static int __init modparams2_init(void)
 {
 	pr_info("%s: inserted\n", OUR_MODNAME);
 	if (mp_debug_level > 0)
 		pr_info("module parameters passed: "
-		"mp_debug_level=%d mp_strparam=%s\ncontrol1=%d\n",
-		mp_debug_level, mp_strparam, control1);
+		"mp_debug_level=%d mp_strparam=%s\ncontrol_freak=%d\n",
+		mp_debug_level, mp_strparam, control_freak);
 
-	/* param 'control1': if it hasn't been passed (implicit guess), or is
+	/* param 'control_freak': if it hasn't been passed (implicit guess), or is
 	 * the same old value, or isn't within the right range,
 	 * it's Unacceptable!
 	 */
-	if ((control1 < 1) || (control1 > 5)) {
+	if ((control_freak < 1) || (control_freak > 5)) {
 		pr_warn("%s: Must pass along module parameter"
-			" 'control1', value in the range [1-5]; aborting...\n",
+			" 'control_freak', value in the range [1-5]; aborting...\n",
 			OUR_MODNAME);
 		return -EINVAL;
 	}
 	return 0;	/* success */
 }
 
-static void __exit modparams1_exit(void)
+static void __exit modparams2_exit(void)
 {
 	if (mp_debug_level > 0)
 		pr_info("module parameters passed: mp_debug_level=%d mp_strparam=%s\n",
@@ -69,5 +69,5 @@ static void __exit modparams1_exit(void)
 	pr_info("%s: removed\n", OUR_MODNAME);
 }
 
-module_init(modparams1_init);
-module_exit(modparams1_exit);
+module_init(modparams2_init);
+module_exit(modparams2_exit);
