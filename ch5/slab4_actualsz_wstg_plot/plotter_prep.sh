b5f1a6973666bceaa8f000536721803c25563745
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-04-11 10:40:01 +0530

    plotter_prep.sh: minor comments
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

--- ch5/slab4_actualsz_wstg_plot/plotter_prep.sh
+++ ch5/slab4_actualsz_wstg_plot/plotter_prep.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-# Prepare the data file from kernel log
+# plotter_prep.sh
+# Book: Linux Kernel Development Cookbook, Kaiwan N Billimoria, Packt.
+# Part of the ch5/slab4_actualsz_wstg_plot code.
+#
+# Script to help prepare the data file from kernel log.
 # We assume that:
 # a) sudo dmesg -C   ; was done prior to the kernel module being inserted
-#  (clearing any the stale stuff)
-# b) the cut(1) below gets rid of the dmesg timestamp; we assume it's there
+#    (clearing any the stale stuff)
+# b) the cut(1) below gets rid of the dmesg timestamp; we assume it's
+#    (timestamp) there
 # c) you will comment out or delete any extraneous lines in the final o/p
 # file 2plotdata.txt after this :-)
 # (To save the trouble, we've (also) kept the 2plotdata.txt file in the repo).
