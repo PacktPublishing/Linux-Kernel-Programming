00acb56b736b15ec09916f13f2c452c3434a6e2a
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-04-27 11:48:39 +0530

    solutions_to_assgn/ch10/miscdrv_rdwr_mutextrylock/: initial commit

---
+++ solutions_to_assgn/ch10/miscdrv_rdwr_mutextrylock/cr8devnode.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# cr8devnode.sh
+# Simple utility script to create the device node for the 'misc'
+# class device driver
+name=$(basename $0)
+OURMODNAME="miscdrv_rdwr_mutextrylock"
+
+MAJOR=10   # misc class is always major # 10
+unalias dmesg 2>/dev/null
+MINOR=$(dmesg |grep "${OURMODNAME}\:minor\=" |cut -d"=" -f2)
+[ -z "${MINOR}" ] && {
+  echo "${name}: failed to retreive the minor #, aborting ..."
+  exit 1
+}
+echo "minor number is ${MINOR}"
+
+sudo mknod /dev/miscdrv c ${MAJOR} ${MINOR}
+ls -l /dev/miscdrv
+exit 0
