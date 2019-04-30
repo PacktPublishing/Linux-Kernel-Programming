a284e06d31b918cb5070d9ec73077f1986454418
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-04-30 16:24:21 +0530

    ch10/3_miscdrv_rdwr_spinlock_pvtdata: ic

---
+++ ch10/3_miscdrv_rdwr_spinlock_pvtdata/cr8devnode.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# cr8devnode.sh
+# Simple utility script to create the device node for the miscdrv_rdwr 'misc'
+# class device driver
+name=$(basename $0)
+OURMODNAME="miscdrv_rdwr_spinlock_pvtdata"
+
+MAJOR=10   # misc class is always major # 10
+unalias dmesg 2>/dev/null
+MINOR=$(dmesg |grep "${OURMODNAME}\:minor\=" |cut -d"=" -f2)
+[ -z "${MINOR}" ] && {
+  echo "${name}: failed to retreive the minor #, aborting ..."
+  exit 1
+}
+echo "minor number is ${MINOR}"
+
+sudo rm -f /dev/miscdrv   # rm any stale instance
+sudo mknod /dev/miscdrv c ${MAJOR} ${MINOR}
+ls -l /dev/miscdrv
+exit 0
