48ac81c609c28a3dec14618bb4193693565392d0
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-06-10 10:02:05 +0530

    ch4/current_affairs: add {e}uid disp, minor updts

--- ch4/current_affairs/current_affairs.c
+++ ch4/current_affairs/current_affairs.c
@@ -8,7 +8,7 @@
  *  GitHub repository:
  *  https://github.com/PacktPublishing/Linux-Kernel-Development-Cookbook
  *
- * From: Ch 3: Kernel and Memory Management Internals -Essentials
+ * From: Ch 4: Kernel and Memory Management Internals -Essentials
  ****************************************************************
  * Brief Description:
  *
@@ -16,28 +16,40 @@
  */
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/sched.h>  /* current */
+#include <linux/sched.h>   /* current() */
+#include <linux/preempt.h> /* in_interrupt() */
+#include <linux/cred.h>    /* current_{e}{u,g}id() */
+#include <linux/uidgid.h>  /* {from,make}_kuid() */
 
 #define OURMODNAME   "current_affairs"
 
-MODULE_AUTHOR("<insert your name here>");
-MODULE_DESCRIPTION("LKDC book:ch/: hello, world");
+MODULE_AUTHOR("Kaiwan N Billimoria");
+MODULE_DESCRIPTION("LKDC book:ch4/current_affairs: display a few members of"
+		" the current process' task structure");
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_VERSION("0.1");
 
 static inline void show_ctx(char *nm)
 {
+	unsigned int uid = from_kuid(&init_user_ns, current_uid());
+	unsigned int euid = from_kuid(&init_user_ns, current_euid());
+
 	pr_info("%s:%s():%d ", nm, __func__, __LINE__);
 	if (likely(!in_interrupt())) {
 		pr_info(
 		"%s: in process context ::\n"
 		" PID         : %6d\n"
 		" TGID        : %6d\n"
-		" name        : %15s\n"
-		" current (ptr to our process context's task_struct) : 0x%pK\n"
-		" stack start : 0x%16pK\n",
-		nm, current->pid, current->tgid, current->comm,
-		current, current->stack);
+		" UID         : %6u\n"
+		" EUID        : %6u (%s root)\n"
+		" name        : %s\n"
+		" current (ptr to our process context's task_struct) :\n"
+		"               0x%pK (0x%16lx)\n"
+		" stack start : 0x%pK (0x%016lx)\n",
+		nm, current->pid, current->tgid, uid, euid,
+		(euid == 0?"have":"don't have"), current->comm,
+		current, (long unsigned)current,
+		current->stack, (long unsigned)current->stack);
 	} else
 		pr_alert("%s: in interrupt context [Should NOT Happen here!]\n",
 			nm);
