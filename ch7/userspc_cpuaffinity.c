af7408aea572f70b3edf5e79aa1e109176300d25
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-06-18 11:41:15 +0530

    userspc_cpuaffinity.c: working ver

--- ch7/userspc_cpuaffinity.c
+++ ch7/userspc_cpuaffinity.c
@@ -1,5 +1,5 @@
 /*
- * ch7/s1/s1.c
+ * ch7/userspc_cpuaffinity/userspc_cpuaffinity.c
  ***************************************************************
  * This program is part of the source code released for the book
  *  "Linux Kernel Development Cookbook"
@@ -22,42 +22,95 @@
 #include <sys/types.h>
 #include <sched.h>
 
-static void disp_cpumask(cpu_set_t *cpumask)
-{
-		int i;
+static unsigned int numcores;
 
-		printf("CPU core# : ");
-		for (i=0; i<CPU_COUNT(cpumask); i++)
-				printf("%d", i);
-		printf("\n            ");
-		for (i=0; i<CPU_COUNT(cpumask); i++) {
-				printf("%u", CPU_ISSET(i, cpumask));
-		}
-		printf("\n");
+static void disp_cpumask(pid_t pid, cpu_set_t *cpumask, unsigned int ncores)
+{
+	int i;
+	char tmpbuf[128];
+
+	printf("CPU affinity mask for PID %d:\n", pid);
+	snprintf(tmpbuf, 127, "ps aux |awk '$2 == %d {print $0}'", pid);
+	system(tmpbuf);
+
+	printf("             +");
+	for (i=0; i<ncores; i++)
+		printf("-");
+	printf("+\n");
+
+	printf(" core#       |");
+	for (i=0; i<ncores; i++)
+		printf("%d", i);
+
+	printf("|\n cpu mask    |");
+	for (i=0; i<ncores; i++) {
+		printf("%u", CPU_ISSET(i, cpumask));
+	}
+	printf("|\n             +");
+	for (i=0; i<ncores; i++)
+		printf("-");
+	printf("+\n");
 }
 
-static int query_sched(void)
+static int query_cpu_affinity(pid_t pid)
 {
-		cpu_set_t cpumask;
+	cpu_set_t cpumask;
 
-		CPU_ZERO(&cpumask);
-		if (sched_getaffinity(0, sizeof(cpu_set_t), &cpumask) < 0) {
-				perror("sched_getaffinity() failed");
-				return -1;
-		}
-		disp_cpumask(&cpumask);
+	CPU_ZERO(&cpumask);
+	if (sched_getaffinity(pid, sizeof(cpu_set_t), &cpumask) < 0) {
+		perror("sched_getaffinity() failed");
+		return -1;
+	}
+	disp_cpumask(pid, &cpumask, numcores);
 
-		return 0;
+	return 0;
+}
+
+static int set_cpu_affinity(pid_t pid, unsigned long bitmask)
+{
+	cpu_set_t cpumask;
+	int i;
+
+	CPU_ZERO(&cpumask);
+
+	/* Iterate over the given bitmask, setting CPU bits as required */
+	for (i=0; i<sizeof(unsigned long)*8; i++) {
+		//printf("bit %d: %d\n", i, (bitmask >> i) & 1);
+		if ((bitmask >> i) & 1)
+			CPU_SET(i, &cpumask);
+	}
+
+	if (sched_setaffinity(pid, sizeof(cpu_set_t), &cpumask) < 0) {
+		perror("sched_setaffinity() failed");
+		return -1;
+	}
+	disp_cpumask(pid, &cpumask, numcores);
+
+	return 0;
 }
 
 int main (int argc, char **argv)
 {
-/*	if (argc < 2) {
-		fprintf(stderr, "Usage: %s \n", argv[0]);
+	pid_t pid = getpid();
+	unsigned long new_cpumask = 0x0;
+
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s number-of-CPU-cores "
+		"[PID] [new-CPU-mask]\n"
+		"(If using the optional params, you must at least pass"
+		" the process PID;\nwe (attempt to) set CPU affinity only if"
+		" new-CPU-mask is passed)\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-*/
-		query_sched();
+	numcores = atoi(argv[1]);
+	if (argc >= 3)
+		pid = atoi(argv[2]);
 
-		exit(EXIT_SUCCESS);
+	query_cpu_affinity(pid);
+	if (argc == 4) {
+		new_cpumask = strtoul(argv[3], 0, 0);
+		set_cpu_affinity(pid, new_cpumask);
+	}
+
+	exit(EXIT_SUCCESS);
 }
