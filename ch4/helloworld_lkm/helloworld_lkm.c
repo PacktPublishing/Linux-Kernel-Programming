9d4fd293217b57fd20b8ed821f573e3942beb629
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-11-25 13:29:35 +0530

    ch4

---
+++ ch4/helloworld_lkm/helloworld_lkm.c
@@ -0,0 +1,40 @@
+/*
+ * ch4/helloworld_lkm/helloworld_lkm.c
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Learn Linux Kernel Development"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *  GitHub repository:
+ *  https://github.com/PacktPublishing/Learn-Linux-Kernel-Development
+ *
+ * From: Ch 4: Writing your First Kernel Module - LKMs Part 1
+ ****************************************************************
+ * Brief Description:
+ * Our very first kernel module, the 'Hello, world' of course! The
+ * idea being to explain the essentials of the Linux kernel's LKM
+ * framework.
+ *
+ * For details, please refer the book, Ch 4.
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_AUTHOR("<insert your name here>");
+MODULE_DESCRIPTION("LLKD book:ch4/helloworld_lkm: hello, world, our first LKM");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_VERSION("0.1");
+
+static int __init hello_llkd_init(void)
+{
+	printk(KERN_INFO "Hello, world\n");
+	return 0;		/* success */
+}
+
+static void __exit hello_llkd_exit(void)
+{
+	printk(KERN_INFO "Goodbye, world\n");
+}
+
+module_init(hello_llkd_init);
+module_exit(hello_llkd_exit);
---
+++ ch4/printk_loglvl/printk_loglvl.c
@@ -0,0 +1,49 @@
+/*
+ * ch4/printk_loglvl/printk_loglvl.c
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Learn Linux Kernel Development"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *  GitHub repository:
+ *  https://github.com/PacktPublishing/Learn-Linux-Kernel-Development
+ *
+ * From: Ch 4: Writing your First Kernel Module - LKMs Part 1
+ ****************************************************************
+ * Brief Description:
+ * Quick test to see kernel printk's at all 8 log levels; when run on a
+ * console, only those messages < current (console) log level (seen as the
+ * first integer in the output of /proc/sys/kernel/printk) will appear on
+ * the console device.
+ *
+ * For details, please refer the book, Ch 4.
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_AUTHOR("<insert your name here>");
+MODULE_DESCRIPTION("LLKD book:ch2/printk_loglvl: print at each kernel log level");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_VERSION("0.1");
+
+static int __init hello_init(void)
+{
+	pr_emerg("Hello, world @ log-level KERN_EMERG   [0]\n");
+	pr_alert("Hello, world @ log-level KERN_ALERT   [1]\n");
+	pr_crit("Hello, world @ log-level KERN_CRIT    [2]\n");
+	pr_err("Hello, world @ log-level KERN_ERR     [3]\n");
+	pr_warn("Hello, world @ log-level KERN_WARNING [4]\n");
+	pr_notice("Hello, world @ log-level KERN_NOTICE  [5]\n");
+	pr_info("Hello, world @ log-level KERN_INFO    [6]\n");
+	pr_debug("Hello, world @ log-level KERN_DEBUG   [7]\n");
+
+	return 0; /* success */
+}
+
+static void __exit hello_exit(void)
+{
+	pr_info("Goodbye, world @ log-level KERN_INFO    [6]\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
