e496c61bbad010c1ebe5f186dc50ef00739820e1
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-01-27 17:32:34 +0530

    cr8devnode.sh: updt

--- ch12/ioctl_intf/userspace_ioctl/cr8devnode.sh
+++ ch12/ioctl_intf/userspace_ioctl/cr8devnode.sh
@@ -10,11 +10,10 @@ DEVNM=/dev/ioctl_intf
 MAJOR=$(grep "${OURMODNAME}" /proc/devices |awk '{print $1}')
 [ -z "${MAJOR}" ] && {
   echo "${name}: failed to retreive the major #, aborting ...
-  (is the driver loaded ?)"
+  (is the ${OURMODNAME} device driver loaded ?)"
   exit 1
 }
 echo "major number is ${MAJOR}"
-
 sudo rm -f "${DEVNM}"     # rm any stale instance
 sudo mknod "${DEVNM}" c ${MAJOR} 0
 ls -l "${DEVNM}"
