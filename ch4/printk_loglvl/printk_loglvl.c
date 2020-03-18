d3c13b2793d81e0ec9735076ed3ee40c9633d66a
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-03-18 16:21:37 +0530

    printk_loglvl.c: add the pr_devel()

--- ch4/printk_loglvl/printk_loglvl.c
+++ ch4/printk_loglvl/printk_loglvl.c
@@ -11,10 +11,10 @@
  * From: Ch 4: Writing your First Kernel Module - LKMs Part 1
  ****************************************************************
  * Brief Description:
- * Quick test to see kernel printk's at all 8 log levels; when run on a
- * console, only those messages < current (console) log level (seen as the
- * first integer in the output of /proc/sys/kernel/printk) will appear on
- * the console device.
+ * Quick test to see kernel printk's at all 8 log levels (and the pr_devel());
+ * when run on a console, only those messages < current (console) log level
+ * (seen as the first integer in the output of /proc/sys/kernel/printk) will
+ * appear on the console device.
  *
  * For details, please refer the book, Ch 4.
  */
@@ -37,6 +37,8 @@ static int __init printk_loglvl_init(void)
 	pr_notice("Hello, world @ log-level KERN_NOTICE  [5]\n");
 	pr_info("Hello, world @ log-level KERN_INFO    [6]\n");
 	pr_debug("Hello, world @ log-level KERN_DEBUG   [7]\n");
+	pr_devel("Hello, world via the pr_devel() macro"
+		" (eff @KERN_DEBUG) [7]\n");
 
 	return 0; /* success */
 }
