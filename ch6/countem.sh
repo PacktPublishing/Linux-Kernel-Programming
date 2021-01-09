5cb7bbe5695f259eb9c5fdd7dff744644b36712b
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2021-01-09 10:18:39 +0530

    enhanced

--- ch6/countem.sh
+++ ch6/countem.sh
@@ -15,13 +15,24 @@
 # * For details, please refer the book, Ch 6.
 # ****************************************************************
 echo "System release info:"
-lsb_release -a 2>/dev/null
+which lsb_release >/dev/null
+if [ $? -eq 0 ] ; then
+  lsb_release -a 2>/dev/null
+else
+  [ -f /etc/issue ] && cat /etc/issue
+  [ -f /etc/os-release ] && cat /etc/os-release
+fi
 
 total_prcs=$(ps -A|wc -l)
 printf "\nTotal # of processes alive              = %9d\n" ${total_prcs}
+
+# ps -LA shows all threads
 total_thrds=$(ps -LA|wc -l)
 printf "Total # of threads alive                = %9d\n" ${total_thrds}
+
+# ps aux shows all kernel threads names (col 11) in square brackets; count 'em
 total_kthrds=$(ps aux|awk '{print $11}'|grep "^\["|wc -l)
+
 printf "Total # of kernel threads alive         = %9d\n" ${total_kthrds}
 printf "Thus, total # of usermode threads alive = %9d\n" $((${total_thrds}-${total_kthrds}))
 
