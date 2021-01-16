b2ccf25f1e4f140f44d7a87c90ad2dab72f714b3
Kaiwan N Billimoria (rpi3b) <kaiwan.billimoria@gmail.com>
2021-01-16 09:11:33 +0530

    needs root

--- solutions_to_assgn/user_kernel_comm/sysfs_addrxlate/addrxlate.sh
+++ solutions_to_assgn/user_kernel_comm/sysfs_addrxlate/addrxlate.sh
@@ -17,7 +17,7 @@ SYSFS_FILE=/sys/devices/platform/llkd_sysfs_addrxlate/addrxlate_kva2pa
 
 usage()
 {
-  echo "Usage: ${name} -k|-p address-to-translate (in hex)
+  echo "Usage: ${name} -k|-p address-to-translate (in hex 0x...)
 
  Option: -k
   Translate the provided kernel virtual address (kva) (2nd parameter) to physical addr
@@ -26,13 +26,13 @@ usage()
   it must lie within the kernel 'lowmem' segment, i.e., between PAGE_OFFSET
   and high_memory
   Eg. on a 32-bit system with a 3:1 VM split:
-    ${name} 0xc000a000      <-- specify addr in hex pl
+    ${name} -k 0xc000a000      <-- specify addr in hex pl
 
  Option: -p
   Translate the provided physical address (2nd parameter) to it's corresponding
   kernel virtual address (kva)
   Eg. on a 32-bit system with a 3:1 VM split:
-    ${name} 0xa000      <-- specify addr in hex pl"
+    ${name} -p 0xa000      <-- specify addr in hex pl"
 }
 
 setup()
@@ -67,7 +67,8 @@ echo "[+] Translating kva --> pa
 "
 
 # Write the kva to transalte to pa to appropriate sysfs file (as root)
-sudo sh -c "echo $1 > /sys/devices/platform/llkd_sysfs_addrxlate/addrxlate_kva2pa"
+echo $1 > /sys/devices/platform/llkd_sysfs_addrxlate/addrxlate_kva2pa
+#sudo sh -c "echo $1 > /sys/devices/platform/llkd_sysfs_addrxlate/addrxlate_kva2pa"
 # Read the result and display
 cat /sys/devices/platform/llkd_sysfs_addrxlate/addrxlate_kva2pa
 }
@@ -83,12 +84,18 @@ echo "[+] Translating pa --> kva
 "
 
 # Write the pa to transalte to kva to appropriate sysfs file (as root)
-sudo sh -c "echo $1 > /sys/devices/platform/llkd_sysfs_addrxlate/addrxlate_pa2kva"
+echo $1 > /sys/devices/platform/llkd_sysfs_addrxlate/addrxlate_pa2kva
+#sudo sh -c "echo $1 > /sys/devices/platform/llkd_sysfs_addrxlate/addrxlate_pa2kva"
 # Read the result and display
 cat /sys/devices/platform/llkd_sysfs_addrxlate/addrxlate_pa2kva
 }
 
 ### 'main' here
+
+[ $(id -u) -ne 0 ] && {
+  echo "${name}: must run as root."
+  exit 1
+}
 
 # argument processing
 [ $# -ne 2 ] && {
